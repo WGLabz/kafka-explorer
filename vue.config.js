58d6564506801f657e59b0d4a1d5806bc8226510
bikash_p <bikashnarayan.p@itcinfotech.com>
2021-10-24 09:29:33 +0530

    Build sys in place

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    configureWebpack: config => {
-      if (process.env.NODE_ENV === 'production') {
-        config.output.publicPath = `${process.cwd()}/dist/`
-      }
-    }
-  }
