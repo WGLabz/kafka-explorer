c24cd7fee7fee2365f95475ce01d7ccef4731d84
Bikash Narayn Panda <oksbwn@gmail.com>
2021-11-01 13:36:38 +0530

    Added Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,21 @@
           transition="scale-transition"
           width="40"
         />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
 
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
     </v-app-bar>
+
     <v-main>
       <router-view />
     </v-main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
+const routes = [{ path: "/foo", component: require("./pages/Home") }];
+
+const router = new VueRouter({
+  routes, // short for `routes: routes`
+});
 new Vue({
   vuetify,
+  router: router,
   render: (h) => h(App),
 }).$mount("#app");
